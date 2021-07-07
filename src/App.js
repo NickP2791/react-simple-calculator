261876e8dc4dd3f7f69e94231f3e11886fa8864e
unknown <admin@roxanick.com>
2021-07-07 14:47:08 -0500

    final fixes

--- src/App.js
+++ src/App.js
@@ -1,58 +1,6 @@
-import React, { useEffect, useState, useReducer } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-const initialState = {
-  countAcc: 0,
-  recOper: "",
-};
-const reducer = (state, action) => {
-  const operators = {
-    "+": function (a, b) {return a + b},
-    "-": function (a, b) {return a - b},
-    "*": function (a, b) {return a * b},
-    "/": function (a, b) {return a / b},
-  };
-  switch (action.type) {
-
-    case "divide":
-      return {
-        countAcc: state.recOper === ""
-            ? state.countAcc + action.value
-            : operators[state.recOper](state.countAcc, action.value),
-        recOper: "/",
-      };
-    case "multiply":
-      return {
-        countAcc: state.recOper === ""
-            ? state.countAcc + action.value
-            : operators[state.recOper](state.countAcc, action.value),
-        recOper: "*",
-      };
-    case "subtract":
-      return {
-        countAcc: state.recOper === ""
-            ? state.countAcc + action.value
-            : operators[state.recOper](state.countAcc, action.value),
-        recOper: "-",
-      };
-    case "add":
-      return {
-        countAcc: state.recOper === ""
-            ? state.countAcc + action.value
-            : operators[state.recOper](state.countAcc, action.value),
-        recOper: "+",
-      };
-    case "equals":
-      return {
-        countAcc: operators[state.recOper](state.countAcc, action.value),
-        recOper: "",
-      };
-    case "erase":
-      return initialState;
-    default:
-      return state;
-  }
-};
 
 function App() {
   // previous and current are used in the displays, recentNum is a placeholder for the last number entered
@@ -61,7 +9,8 @@ function App() {
   const [previous, setPrevious] = useState("");
   const [current, setCurrent] = useState("0");
   const [recentNum, setRecentNum] = useState("");
-  const [subtotal, dispatch] = useReducer(reducer, initialState);
+  const [operator, setOperator] = useState("");
+  const [subtotal2, setSubTotal2] = useState(0)
 
   const handleNumber = ({ target: { value } }) => {
     let pattern = new RegExp(/^-?\d+\.?\d*$/);
@@ -79,163 +28,147 @@ function App() {
 
     if (pattern.test(str) || str === "-") {
       //test for initial state
-      if (subtotal.recOper === "" && recentNum === "" && current === "0") {
+      if (operator === "" && recentNum === "" && current === "0") {
         setCurrent(str);
          setRecentNum(str);
         // test for initial state after first zero
       } else if (
-        subtotal.recOper === "" && recentNum === "0" && current === "0") {
+        operator === "" && recentNum === "0" && current === "0") {
         setCurrent((prev) => (value === "." ? prev + value : prev));
         setRecentNum((prev) => (value === "." ? prev + value : prev));
         //initial state after an equals was executed
         // disallow typing numbers AFTER an equal has been exectuted
-      } else if (subtotal.countAcc.toString() === current && recentNum === "0") {
+      } else if (subtotal2.toString() === current && recentNum === "0") {
         return;
       } else {
         setCurrent((prev) => prev + value);
         setRecentNum((prev) => prev + value);
       }
-    
     }
   };
 
-  useEffect(() => {
-    //  If AC, clear all
-    if (subtotal.recOper === "" && subtotal.countAcc === 0) {
-      setPrevious(() => "");
-      ClearAllCleanup();
-
-      //if equal, add "" and total to end of previous display,"" is flag
-    } else if (subtotal.recOper === "" && subtotal.countAcc !== "") {
-      setPrevious((prev) => prev + `${recentNum}=${subtotal.countAcc}`);
-      setCurrent(() => `${subtotal.countAcc}`);
-      setRecentNum(() => "0");
-
-      // this is for operations AFTER the equal has been done
-    } else if (subtotal.recOper !== "" && recentNum === "0") {
-      setPrevious(`${subtotal.countAcc}${subtotal.recOper}`);
-      setCurrent(() => "");
-      setRecentNum(() => "");
-    } else {
-      //normal operation, when numbers operations added to previous
-      setPrevious((prev) => prev + `${recentNum}${subtotal.recOper}`);
-      setCurrent(() => "");
-      setRecentNum(() => "");
-    }
-  }, [subtotal]);
-
-//Used for cleaning up double operation signs
-  useEffect(() => {
-    ReplaceOperations();
-  }, [previous]);
-
-  const ReplaceOperations = () => {
-    setPrevious(
-      previous.replace(/[-/*+]\+/, "+")
+  const ReplaceOperations = (fixing) => {
+    console.log(fixing)
+     return (
+      fixing.replace(/[-/*+]\+/, "+")
     .replace(/[-+*/]\//, "/")
     .replace(/[-/+*]\*/, "*")
     .replace("--", "+").replace("+-", "-")
-    );
+    )
   }
 
   const ClearAllCleanup = () => {
-    setCurrent(() => "0");
-    setRecentNum(() => "");
-  };
+    setPrevious( "")
+    setCurrent("0");
+    setRecentNum( "");
+    setSubTotal2(0)
+    setOperator("")
+    };
 
-  // function called when consecutive operation signs are present, this part needed
-  //for when an operation needs to be sent to ussReducer and not change the subtotal amount
-  const SignSwitchTest =() => {
-    return (subtotal.recOper === "-" || subtotal.recOper === "+") ? 0 : 1;
+  const justDoIt = (operation) => {
+        const operators = {
+      "+": function (a, b) {return a + b},
+      "-": function (a, b) {return a - b},
+      "*": function (a, b) {return a * b},
+      "/": function (a, b) {return a / b},
+    };
+
+    let subT = operators[operator](subtotal2, +recentNum);
+    if (operation === "=") {
+      setSubTotal2(subT);
+      setOperator("=");
+      setRecentNum("");
+      setPrevious((prev) =>
+        ReplaceOperations(prev + `${recentNum}${operation}${subT}`)
+      );
+      setCurrent(() => subT);
+    } else {
+      setSubTotal2(subT);
+      setOperator(operation);
+      setRecentNum("")
+      setPrevious((prev) =>
+        ReplaceOperations(prev + `${recentNum}${operation}`)
+      );
+      setCurrent(() => "");    
+    }     
+  } 
+  
+  const handleOperations = ({ target: { value } }) => {
+  let pat =/[=+*/]/
+    if (previous === "" ) {
+      setSubTotal2(prev => prev + +recentNum)
+      setOperator(value);
+      setRecentNum("");
+      setPrevious((prev) =>
+              ReplaceOperations(prev + `${recentNum}${value}`)
+            );
+      setCurrent(() => "");
+    } else if (pat.test(operator) && (recentNum === ""|| recentNum === "-")) {
+     setPrevious(subtotal2 + value)
+      setOperator(value);
+     setCurrent(() => "");
+     setRecentNum("")
+    } else {
+       justDoIt(value)
+    }}
+
+ const handleEqual =() => {
+    if (recentNum ==="" && operator ==="") {
+      return;
+    } else if (recentNum !=="" && operator ===""){
+      setPrevious(`${recentNum}=${recentNum}`)
+    } else if (recentNum ==="" && operator !==""){
+      setPrevious((prev) =>ReplaceOperations (prev + `${recentNum}=${subtotal2}`));
+      setCurrent(() => `${subtotal2}`);
+      setRecentNum(() => "0");
+    } else {
+      justDoIt("=")
+    }
   }
 
   return (
     <div className='App'>
-
-{/* //       Used in development
-//       <br />
-//       <br />
-//       <br />
-//       subtotal {subtotal.countAcc}
-//       <br />
-//       Recent operation {subtotal.recOper} <br />
-//       Recent Number {recentNum} */}
-
+      {/* Used in development
+      <br />
+      <br />
+      <br />
+      subtotal {subtotal2}
+      <br />
+      Recent operation {operator} <br />
+      Recent Number {recentNum} */}
+      
       <div className='grid-container'>
         <div className='display display-main span-4'>{previous}</div>
-        <div className='display display-second span-4' id='display'>{current}</div>
-        <button className='function clear span-2' id='clear'
-          onClick={
-            previous ? () => dispatch({ type: "erase" }) : ClearAllCleanup
-          }>
-          AC
-        </button>
-        <button className='function' id='divide'
-          onClick={ recentNum === "" || recentNum === "-"
-              ? () => dispatch({ type: "divide", value: SignSwitchTest() })
-              : () =>
-                  dispatch({
-                    type: "divide",
-                    value: recentNum === "0" ? 0 : +current,
-                  })
-          }>
-          /
-        </button>
-        <button className='function' id='multiply'
-          onClick={ recentNum === "" || recentNum === "-"
-              ? () => dispatch({ type: "multiply", value: SignSwitchTest() })
-              : () =>
-                  dispatch({
-                    type: "multiply",
-                    value: recentNum === "0" ? 0 : +current,
-                  })
-          }>
-          X
-        </button>
+        <div className='display display-second span-4' id='display'>
+          {current}
+        </div>
+        <button className='function clear span-2' id='clear' onClick={ClearAllCleanup}>AC</button>
+        <button className='function' id='divide' value='/' onClick={handleOperations}>/</button>
+        <button className='function' id='multiply' value='*' onClick={handleOperations}>X</button>
         <button className='numbers' id='seven' value='7' onClick={handleNumber}>7</button>
         <button className='numbers' id='eight' value='8' onClick={handleNumber}>8</button>
         <button className='numbers' id='nine' value='9' onClick={handleNumber}>9</button>
-        {/* ################################################################# */}
+        {/* ################################################## */}
         <button className='function' id='subtract' value='-'
           onClick={
-            subtotal.recOper === "-" && recentNum === "-"
+            operator === "-" && recentNum === "-"
               ? ""
-              : recentNum === ""
-              ? handleNumber
-              : recentNum === "-"
-              ? () => dispatch({ type: "subtract", value: SignSwitchTest() })
-              : () =>
-                  dispatch({
-                    type: "subtract",
-                    value: recentNum === "0" ? 0 : +current,
-                  })
+              : operator === "" || operator === "="|| recentNum !==""
+              ? handleOperations
+              : handleNumber
           }>
           -
         </button>
-        {/* ################################################################# */}
+        {/* ################################################# */}
         <button className='numbers' id='four' value='4' onClick={handleNumber}>4</button>
         <button className='numbers five' id='five' value='5' onClick={handleNumber}>5</button>
         <button className='numbers six' id='six' value='6' onClick={handleNumber}>6</button>
-        <button className='function add' id='add'
-          onClick={ recentNum === "" || recentNum === "-"
-              ? () => dispatch({ type: "add", value: SignSwitchTest() })
-              : () =>
-                  dispatch({
-                    type: "add",
-                    value: recentNum === "0" ? 0 : +current,
-                  })
-          }>
-          +
-        </button>
+        <button className='function add' id='add' value='+'
+          onClick={handleOperations}>+</button>
         <button className='numbers one' id='one' value='1' onClick={handleNumber}>1</button>
         <button className='numbers two' id='two' value='2' onClick={handleNumber}>2</button>
         <button className='numbers three' id='three' value='3' onClick={handleNumber}>3</button>
-        <button className='function equal span-2v' id='equals' value='='
-          onClick={() =>
-            subtotal.recOper && dispatch({ type: "equals", value: +current })
-          }>
-          =
-        </button>
+        <button className='function equal span-2v' id='equals' value='=' onClick={handleEqual}>=</button>
         <button className='numbers zero span-2' id='zero' value='0' onClick={handleNumber}>0</button>
         <button className='numbers decimal' id='decimal' value='.' onClick={handleNumber}>.</button>
       </div>
